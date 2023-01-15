258d7475d1cf0191bc843d3fab9e2a89a0771876
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-15 18:00:02 -0300

    feat: home page structure made

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom"
+import logo from "../../assets/Nu Kenzie.png"
+import cardhome from "../../assets/cardhome2.png"
 const Home = () => {
-    return <h1>Home</h1>
+    return (
+        <>
+        <section>
+            <header>
+                <img src={logo} alt="logo"/>
+                <h1>Centralize o controle das suas finanças</h1>
+                <h3>de forma rápida e segura</h3>
+                <Link to={"/dashboard"}>Iniciar</Link>
+            </header>
+            <div>
+                <img src={cardhome} alt="imagem de card dashboard" />
+            </div>
+        </section>
+
+        </>
+    )
 }
 
 export default Home
