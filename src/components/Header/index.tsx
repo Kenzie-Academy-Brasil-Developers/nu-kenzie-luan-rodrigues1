8760fe43936ec0b534f696b94e029b810836651c
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-01-15 11:18:00 -0300

    feat: created component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <img src="" alt="Logo" />
+                <button>Inicio</button>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
